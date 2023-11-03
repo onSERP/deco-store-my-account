2373b3e595717bab698de097c5aea2b8a737f4e8
Pedro de Jesus <web.pedrodejesus@gmail.com>
2023-11-03 13:53:54 -0300

    user data form finshed

--- components/my-account/UserData.tsx
+++ components/my-account/UserData.tsx
@@ -45,7 +45,7 @@ function UserData({title, data}: Props) {
       <a id="user-data-edit" aria-label="Editar Informações" href="#" class="btn btn-primary uppercase">Editar Informações</a>
       <a aria-label="Alterar Senha" href="#" class="btn btn-primary uppercase ml-4">Alterar Senha</a>
     </div>
-    <UserDataEdit title={title} data={data} />
+    <UserDataEdit title="Editar informações" data={data} />
   </>);
 }
 
@@ -53,16 +53,61 @@ function UserDataEdit({title, data}: Props) {
   return (<div id="user-data-edit-form" class="hidden">
       <Title content={title} size="medium" />
       <div>
-        <div class="mb-2">
-          <input type="text" placeholder={`${data.name? data.name : "Nome Completo"}`} class="input input-bordered w-full max-w-xs" />
+        <div class="form-control w-full max-w-xs">
+          <label class="label">
+            <span class="label-text uppercase font-bold">Nome</span>
+          </label>
+          <input type="text" placeholder="Nome" value={`${data.name? data.name : ""}`} class="input input-bordered w-full max-w-xs" />
         </div>
 
-        <div class="mb-2">
-        <input type="text" placeholder={`${data.email? data.email : "E-mail"}`} class="input input-bordered w-full max-w-xs" />
+        <div class="form-control w-full max-w-xs">
+          <label class="label">
+            <span class="label-text uppercase font-bold">E-mail</span>
+          </label>
+          <input type="text" placeholder="E-mail" value={`${data.email? data.email : ""}`} class="input input-bordered w-full max-w-xs" />
+        </div>
+
+        <div class="form-control w-full max-w-xs">
+          <label class="label">
+            <span class="label-text uppercase font-bold">Nome que deseja ser chamado(a)</span>
+          </label>
+          <input type="text" placeholder="Nome que deseja ser chamado(a)" value={`${data.friendlyName? data.friendlyName : ""}`} class="input input-bordered w-full max-w-xs" />
         </div>
       </div>
 
-      <a aria-label="Salvar Informações" href="#" class="btn btn-primary uppercase">Salvar Informações</a>
+      <div class="form-control w-full max-w-xs">
+          <label class="label">
+            <span class="label-text uppercase font-bold">Sexo</span>
+          </label>
+          <select class="select select-bordered w-full max-w-xs">
+            <option value="Masculino" selected={data.gender === "Masculino"}>Masculino</option>
+            <option value="Feminino" selected={data.gender === "Feminino"}>Feminino</option>
+            <option value="Outro" selected={data.gender === "Outro"}>Outro</option>
+          </select>
+        </div>
+
+        <div class="form-control w-full max-w-xs">
+          <label class="label">
+            <span class="label-text uppercase font-bold">CPF</span>
+          </label>
+          <input type="text" placeholder="CPF" value={`${data.document? data.document : ""}`} class="input input-bordered w-full max-w-xs" />
+        </div>
+
+        <div class="form-control w-full max-w-xs mb-4">
+          <label class="label">
+            <span class="label-text uppercase font-bold">Telefone</span>
+          </label>
+          <input type="text" placeholder="Telefone" value={`${data.phone? data.phone : ""}`} class="input input-bordered w-full max-w-xs" />
+        </div>
+
+        <div class="form-control flex content-center mb-4">
+          <label class="cursor-pointer">
+            <input type="checkbox" checked="checked" class="checkbox" />
+            <span class="label-text ml-2">Quero receber e-mails com novidades</span> 
+          </label>
+        </div>
+
+      <a aria-label="Salvar Alterações" href="#" class="btn btn-primary uppercase">Salvar Alterações</a>
       <a id="user-data-edit-return" aria-label="Voltar" href="#" class="btn btn-primary uppercase ml-4">Voltar</a>
     </div>
   );
